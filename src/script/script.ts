3d156cf3a3e02ed7ae063f2289938cd01db9c08e
andre <andresntlcs@gmail.com>
2024-01-28 19:34:52 +0800

    image added to item card

--- src/script/script.ts
+++ src/script/script.ts
@@ -3,61 +3,85 @@ const menu = [
         title: "Signature Breakfast",
         category: "breakfast",
         price: 10,
+        img: "../../src/assets/breakfast-base.png",
+        desc: "lorem ipsum",
     },
     {
         title: "Pancakes",
         category: "breakfast",
         price: 10,
+        img: "../../src/assets/breakfast-pancakes.png",
+        desc: "lorem ipsum",
     },
     {
         title: "Egg Toast",
         category: "breakfast",
         price: 10,
+        img: "../../src/assets/breakfast-egg_toast.png",
+        desc: "lorem ipsum",
     },
     {
         title: "Classic Burger",
         category: "meal",
         price: 10,
+        img: "../../src/assets/meal-burger.png",
+        desc: "lorem ipsum",
     },
     {
         title: "Fried Chicken",
         category: "meal",
         price: 10,
+        img: "../../src/assets/meal-chicken.png",
+        desc: "lorem ipsum",
     },
     {
         title: "Spaghetti",
         category: "meal",
         price: 10,
+        img: "../../src/assets/meal-spaghetti.png",
+        desc: "lorem ipsum",
     },
     {
         title: "Chicken Tenders",
         category: "sides",
         price: 10,
+        img: "../../src/assets/sides-chicken_tenders.png",
+        desc: "lorem ipsum",
     },
     {
         title: "Fries",
         category: "sides",
         price: 10,
+        img: "../../src/assets/sides-fries.png",
+        desc: "lorem ipsum",
     },
     {
         title: "Fish and Chips",
         category: "sides",
         price: 10,
+        img: "../../src/assets/sides-fish_and_chips.png",
+        desc: "lorem ipsum",
     },
     {
         title: "Chocolate Smoothie",
         category: "drinks",
         price: 10,
+        img: "../../src/assets/drinks-choco_smoothie.png",
+        desc: "lorem ipsum",
     },
     {
         title: "Mango Shake",
         category: "drinks",
         price: 10,
+        img: "../../src/assets/drinks-mango_shake.png",
+        desc: "lorem ipsum",
     },
     {
         title: "Strawberry Milkshake",
         category: "drinks",
         price: 10,
+        img: "../../src/assets/drinks-strawberry_milkshake.png",
+        desc: "lorem ipsum",
     },
 ]
 
@@ -65,6 +89,8 @@ interface menuTypes {
     title: string;
     category: string;
     price: number;
+    img: string;
+    desc: string;
 }
 
 const menuSection = document.querySelector('#MenuSection')!;
@@ -86,7 +112,16 @@ window.addEventListener('DOMContentLoaded', function() {
 
 function displayMenu(menu: menuTypes[]){
     let displayMenu: string | string[] = menu.map(function (item) {
-        return `<p class="font-black">${item.title}</p> <p>$${item.price}</p> <br>`
+        return `
+        <div id="MenuDisplay">
+            <img src=${item.img}></img>
+            <header>
+                <h4 class="font-black">${item.title}</h4>
+                <h4>$${item.price}</h4>
+            </header>
+            <p>${item.desc}</p>
+        </div>
+        `
     
        });
     
